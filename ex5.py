cc42c394226b7c300f049246deccd6ad9dd84147
MyoMyoCU <septembermyo96@gmail.com>
2018-06-04 23:07:02 +0630

    Python Exercise 5

---
+++ ex5.py
@@ -0,0 +1,25 @@
+#More Variables and Printing 
+#exercise5 embedded variables in string
+
+
+my_name = ' Myo Myo '
+my_age = 22 
+my_height = 60 # inches
+my_weight = 100 #lbs
+my_eyes = 'Brown '
+my_teeth = 'White'
+my_hair = 'Long Black  ' 
+
+
+print (f"Let's talk about {my_name}." )
+print (f"She's {my_height} inches tall.")
+print (f"She's {my_weight} pounds heavy.")
+print ("Actually that's not too heavy .")
+print (f"She's got {my_eyes} eyes and {my_hair} hair.")
+print (f"Her teeth are usually {my_teeth} depending on the coffee . ")
+
+#this line is tricky , try to get it exactly right 
+
+total = my_age + my_height + my_weight
+
+print (f"If I add {my_age} , {my_height}, and {my_weight} I get {total}.")
