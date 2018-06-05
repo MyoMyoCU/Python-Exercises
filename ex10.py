d023babf9c4ff798f1bb9280c1425391026e360a
MyoMyoCU <septembermyo96@gmail.com>
2018-06-05 16:20:06 +0630

    Python Exercise 10

---
+++ ex10.py
@@ -0,0 +1,16 @@
+#printing
+
+tabby_cat = "\t I'm tabbed in . "
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a\\ cat ."
+
+fat_cat = """ 
+I'll do a list:
+    \t* Cat food
+    \t* fishes
+    \t* Catnip\n\t* Grass
+    """
+print (tabby_cat)
+print (persian_cat)
+print (backslash_cat)
+print (fat_cat)
